a314c25cff8de350dfba4b1f88aa4a6f16f03455
Leonardo Gama <leo10gama@gmail.com>
2021-08-25 18:13:28 -0400

    Update triggers and DB framework
    OK so this was painful but the updating Ryu Numbers *currently* only work if you start by adding a game that immediately links to a character/game, and THEN adding characters from that game. This is because MySQL doesn't like recursive triggers :( but I will work to fix this (*eventually*)

--- init.py
+++ init.py
@@ -1,6 +1,11 @@
 # NOTE: This script should only be run ONCE to initialize the database
 import mysql.connector
 
+# PHRASE CONSTANTS TO USE TO INSERT
+insertCharacter = lambda game_character: "INSERT IGNORE INTO game_character (name) VALUES ('%s')" % game_character
+insertGame = lambda game_title, release_date: "INSERT IGNORE INTO game (title, release_date) VALUES ('%s', '%s')" % (game_title, release_date)
+insertRelation = lambda cname, gtitle: "INSERT IGNORE INTO appears_in (cname, gtitle) VALUES ('%s', '%s')" % (cname, gtitle)
+
 # Create database if not exists
 db1 = mysql.connector.connect(
     host        ="localhost",
@@ -25,38 +30,37 @@ cursor.execute(characterTable)
 gameTable = "CREATE TABLE IF NOT EXISTS game (title VARCHAR(64) NOT NULL, ryu_number INTEGER DEFAULT 99, release_date DATE, PRIMARY KEY (title));"
 cursor.execute(gameTable)
 # Create 'appears_in' relation table
-appearsInTable = "CREATE TABLE IF NOT EXISTS appears_in (cname VARCHAR(64) NOT NULL, gtitle VARCHAR(64) NOT NULL, FOREIGN KEY (cname) REFERENCES game_character(name), FOREIGN KEY (gtitle) REFERENCES game(title));"
+appearsInTable = "CREATE TABLE IF NOT EXISTS appears_in (cname VARCHAR(64) NOT NULL, gtitle VARCHAR(64) NOT NULL, PRIMARY KEY (cname, gtitle), FOREIGN KEY (cname) REFERENCES game_character(name), FOREIGN KEY (gtitle) REFERENCES game(title));"
 cursor.execute(appearsInTable)
 
 # Create the triggers to automatically set the Ryu Numbers
 dropAI = "DROP TRIGGER IF EXISTS update_ai;"
 updateAI = "CREATE TRIGGER update_ai AFTER INSERT ON appears_in FOR EACH ROW BEGIN \
-    IF (SELECT ryu_number FROM game WHERE title=NEW.gtitle) > (SELECT ryu_number FROM game_character WHERE name=NEW.cname) THEN UPDATE game SET ryu_number=(SELECT ryu_number FROM game_character WHERE name=NEW.cname)+1 WHERE title=NEW.gtitle;\
-	ELSEIF (SELECT ryu_number FROM game_character WHERE name=NEW.cname) > (SELECT ryu_number FROM game WHERE title=NEW.gtitle) THEN UPDATE game_character SET ryu_number=(SELECT ryu_number FROM game WHERE title=NEW.gtitle) WHERE name=NEW.cname;\
+    IF (SELECT ryu_number FROM game AS G WHERE G.title=NEW.gtitle) > (SELECT ryu_number FROM game_character AS C WHERE C.name=NEW.cname) THEN \
+        UPDATE game AS G \
+        SET ryu_number=(SELECT ryu_number FROM game_character AS C WHERE C.name=NEW.cname)+1 \
+        WHERE G.title=NEW.gtitle; \
+	ELSEIF (SELECT ryu_number FROM game_character AS C WHERE C.name=NEW.cname) > (SELECT ryu_number FROM game AS G WHERE G.title=NEW.gtitle) THEN \
+        UPDATE game_character AS C \
+        SET ryu_number=(SELECT ryu_number FROM game AS G WHERE G.title=NEW.gtitle) \
+        WHERE C.name=NEW.cname; \
 	END IF; END;"
-dropChar = "DROP TRIGGER IF EXISTS update_char;"
-updateChar = "CREATE TRIGGER update_char \
-AFTER UPDATE ON game_character \
-FOR EACH ROW \
-	UPDATE game \
-    SET ryu_number=NEW.ryu_number+1 \
-    WHERE appears_in.cname=NEW.name AND appears_in.gtitle=title AND ryu_number>(NEW.ryu_number+1);"
-dropGame = "DROP TRIGGER IF EXISTS update_game;"
-updateGame = "CREATE TRIGGER update_game \
-AFTER UPDATE ON game \
-FOR EACH ROW \
-	UPDATE game_character \
-    SET ryu_number=NEW.ryu_number \
-    WHERE appears_in.cname=name AND appears_in.gtitle=NEW.title AND ryu_number>NEW.ryu_number;"
 cursor.execute(dropAI)
 cursor.execute(updateAI)
-cursor.execute(dropChar)
-cursor.execute(updateChar)
-cursor.execute(dropGame)
-cursor.execute(updateGame)
 
 # Add the legendary RYU himself
-cursor.execute("INSERT INTO game_character (name, ryu_number) VALUES ('Ryu', 0)")
+cursor.execute("INSERT IGNORE INTO game_character (name, ryu_number) VALUES ('Ryu', 0)")
+
+### TEMP TEST ENTRIES TO VERIFY TRIGGERS ###
+cursor.execute(insertCharacter("Kratos"))
+cursor.execute(insertCharacter("Heihachi Mishima"))
+cursor.execute(insertGame("Street Fighter X Tekken", "2012-03-06"))
+cursor.execute(insertGame("PlayStation All-Stars Battle Royale", "2012-11-20"))
+cursor.execute(insertRelation("Ryu", "Street Fighter X Tekken"))
+cursor.execute(insertRelation("Heihachi Mishima", "Street Fighter X Tekken"))
+cursor.execute(insertRelation("Heihachi Mishima", "PlayStation All-Stars Battle Royale"))
+cursor.execute(insertRelation("Kratos", "PlayStation All-Stars Battle Royale"))
+### END TEMP ENTRIES ###
 
 mydb.commit()
 mydb.close
