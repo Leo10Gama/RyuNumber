f5f3df3b6d9f838b185bc6d280e753aea1fa366b
Leonardo Gama <leo10gama@gmail.com>
2021-09-06 13:21:40 -0400

    'Add to game' command implemented
    So a few things happened in this commit, but yeah the main thing is this new command.
    Essentially, let's say a game exists in the DB, but new DLC is announced that adds another character (or you missed one by accident). Now you can add them without manually opening that text file!
    In addition, in that command and the 'Insert game' command in the frontend, I streamlined the character insertion process in a separate function.
    Oh and as always, M O R E   D A T A ! ! ! ! !

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ MENU = "\n+------------------RYU DATABASE------------------+\n\
 | (c/C) Query a character (exactly)              |\n\
 | (g/G) Query a game (exactly)                   |\n\
 | (i/I) Insert a game and characters into the DB |\n\
+| (a/A) Add characters to an existing game       |\n\
 | (r/R) Reset the database (include all details) |\n\
 | (p/P) Get a path from a character to Ryu       |\n\
 | (q/Q) Close the database and quit              |\n\
 |                                                |\n\
 |   (Note: brackets in desc. = capital letter)   |\n\
 +------------------------------------------------+\n"
+illegalCharacters = ["/", "\\", ":", "*", "?", "\"", "'", "<", ">", "|", "'", "`", "%"]
 
 def queryCharacter(exact = False):
     limiter = 4     # Limiter will limit how many "appears_in" games show
@@ -48,11 +50,52 @@ def queryGame(exact = False):
         else:
             print("No games by that name could be found")
 
+def addCharacters():
+    charactersToAdd = []
+    c2add = None
+    while not c2add:
+        # Receive input
+        c2add = input("Enter character name, or enter '.' to cancel the insert (enter nothing to finish):\n")
+        for c in illegalCharacters:
+            c2add.replace(c, "")
+        # Query if character already exists
+        if c2add == ".":
+            return []
+        if c2add:
+            possibleCharacters = game_character.getByName(c2add)
+            # If character exists, prompt to pick one of them or the entered value, or some other value entirely
+            if possibleCharacters:
+                print("Found %d character(s) with similar name:\n" % len(possibleCharacters))
+                for i in range(len(possibleCharacters)):
+                    print("(%d) %s" % (i, possibleCharacters[i].name))
+                print("\nWhat would you like to do?\n[num] Use that character name\n[n/N] Use what I wrote\n[anything else] Enter another name\n")
+                whatDo = input()
+                # Number is inputted, use that value
+                if whatDo.isnumeric() and int(whatDo) < len(possibleCharacters):
+                    print("Adding '%s'..." % possibleCharacters[int(whatDo)].name)
+                    charactersToAdd.append(possibleCharacters[int(whatDo)].name)
+                # Use the value I inserted
+                elif whatDo.lower() == "n":
+                    print("Adding '%s'..." % c2add)
+                    charactersToAdd.append(c2add)
+                else:
+                    print("Cancelling that insert...")
+            else:
+                whatDo = input("\n'%s' does not exist in the database yet.\nAdd them anyway? (y/n): " % c2add)
+                if whatDo.lower() in ["y", "ye", "yes", "yea"]:
+                    print("Adding '%s'..." % c2add)
+                    charactersToAdd.append(c2add)
+                else:
+                    print("Cancelling that insert...")
+            c2add = None
+        else:
+            c2add = "owo"
+    return charactersToAdd
+
 def insertGame():
     path = "Games List"
     # newGame contains the name of the game to be inserted
     # Parse it with characters that can be used in text file names
-    illegalCharacters = ["/", "\\", ":", "*", "?", "\"", "'", "<", ">", "|", "'", "`"]
     newGame = input("Enter the game's name: ")
     for c in illegalCharacters:
         newGame.replace(c, "")
@@ -70,34 +113,8 @@ def insertGame():
             releaseDate = None
             print("Format invalid. Please enter the release date in the proper format!")
         # Get characters
-        charactersToAdd = []
-        c2add = None
-        while not c2add:
-            # Receive input
-            c2add = input("Enter character name, or enter '.' to cancel the insert (enter nothing to finish):\n")
-            # Query if character already exists
-            if c2add == ".":
-                return
-            if c2add:
-                possibleCharacters = game_character.getByName(c2add)
-                # If character exists, prompt to pick one of them or the entered value, or some other value entirely
-                if possibleCharacters:
-                    print("Found %d character(s) with similar name:\n" % len(possibleCharacters))
-                    for i in range(len(possibleCharacters)):
-                        print("(%d) %s" % (i, possibleCharacters[i].name))
-                    print("\nWhat would you like to do?\n[num] Use that character name\n[n/N] Use what I wrote\n[anything else] Enter another name")
-                    whatDo = input()
-                    # Number is inputted, use that value
-                    if whatDo.isnumeric() and int(whatDo) < len(possibleCharacters):
-                        charactersToAdd.append(possibleCharacters[int(whatDo)].name)
-                    # Use the value I inserted
-                    elif whatDo.lower() == "n":
-                        charactersToAdd.append(c2add)
-                else:
-                    charactersToAdd.append(c2add)
-                c2add = None
-            else:
-                c2add = "owo"
+        charactersToAdd = addCharacters()
+        if not charactersToAdd: return
         # Write the file
         print("Creating file for game...", end="")
         with open("%s/%s.txt" % (path, newGame), "w") as f:
@@ -115,6 +132,41 @@ def insertGame():
         game_character.insertCharactersToGame(priorityInserts, newGame)
         game_character.insertCharactersToGame(charactersToAdd, newGame)
         print("Done")
+
+def addToGame():
+    path = "Games List"
+    # Get game to add to
+    gameToAddTo = input("Enter game title: ")
+    # Verify game in DB and cross-check with user
+    gameToAddTo = game.getByTitle(gameToAddTo)
+    if not gameToAddTo:
+        print("That game does not exist in the database! Try inserting the game yourself.")
+    else:
+        for i in range(len(gameToAddTo)):
+            print("(%d) %s" % (i, gameToAddTo[i]))
+        gameIndex = input("\nPlease select which game to add to (int): ")
+        if gameIndex.isnumeric() and int(gameIndex) < len(gameToAddTo):
+            gameToAddTo = gameToAddTo[int(gameIndex)].title
+            # Start receiving character input
+            charactersToAdd = addCharacters()
+            if not charactersToAdd: return
+            # Write the file
+            print("Writing to file...", end="")
+            with open("%s/%s.txt" % (path, gameToAddTo), "a") as f:
+                for c in charactersToAdd:
+                    f.write("\n%s" % c)
+            print("Done")
+            # Insert into the database
+            print("Adding to database...", end="")
+            priorityInserts = [x.name for x in game_character.getManyByNames(tuple(charactersToAdd))]
+            for x in priorityInserts:
+                if x in charactersToAdd:
+                    charactersToAdd.remove(x)
+            game_character.insertCharactersToGame(priorityInserts, gameToAddTo)
+            game_character.insertCharactersToGame(charactersToAdd, gameToAddTo)
+            print("Done")
+        else:
+            print("Invalid input. Cancelling action...")
 
 def resetDatabase(detailed = False):
     response = input("This command will take a while to execute.\nAre you sure you want to reset the database? (y/n): ")
@@ -148,6 +200,8 @@ def main():
             queryGame(True if command == "G" else False)
         elif command == "i" or command == "I":
             insertGame()
+        elif command == "a" or command == "A":
+            addToGame()
         elif command == "r" or command == "R":
             resetDatabase(True if command == "R" else False)
         elif command == "p" or command == "P":
